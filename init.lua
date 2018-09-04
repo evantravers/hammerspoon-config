44263b9e66934a6c81d43712160cb2a6a4dcc205
Evan Travers <evantravers@gmail.com>
2018-09-04 14:29:18 -0500

    Reformat table to allow adding layout info

--- init.lua
+++ init.lua
@@ -5,15 +5,42 @@ config = {}
 config.hyper_fall_through = {'c', 'space', '\\', 'p', '\''}
 
 config.applications = {
-  {name = 'iTerm', hyper_shortcut = 'j' },
-  {name = 'Google Chrome', hyper_shortcut = 'k' },
-  {name = 'Dash', hyper_shortcut = 'h' },
-  {name = 'Slack', hyper_shortcut = 'i' },
-  {name = 'Microsoft Outlook', hyper_shortcut = 'e' },
-  {name = 'Finder', hyper_shortcut = 'f' },
-  {name = 'Discord', hyper_shortcut = 'l' },
-  {name = 'zoom.us', hyper_shortcut = 'z' },
-  {name = 'Sketch', hyper_shortcut = 'v' },
+  {
+    name = 'iTerm',
+    hyper_shortcut = 'j'
+  },
+  {
+    name = 'Google Chrome',
+    hyper_shortcut = 'k'
+  },
+  {
+    name = 'Dash',
+    hyper_shortcut = 'h'
+  },
+  {
+    name = 'Slack',
+    hyper_shortcut = 'i'
+  },
+  {
+    name = 'Microsoft Outlook',
+    hyper_shortcut = 'e'
+  },
+  {
+    name = 'Finder',
+    hyper_shortcut = 'f'
+  },
+  {
+    name = 'Discord',
+    hyper_shortcut = 'l'
+  },
+  {
+    name = 'zoom.us',
+    hyper_shortcut = 'z'
+  },
+  {
+    name = 'Sketch',
+    hyper_shortcut = 'v'
+  },
 }
 
 
