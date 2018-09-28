daca2209ecf6a67b2fef88422af8eafd8d19c384
Evan Travers <evantravers@gmail.com>
2018-09-28 09:21:09 -0500

    Add 'hash' style names to my config list
    Thanks @megalithic! I'm so bad at lua. :(

--- init.lua
+++ init.lua
@@ -5,45 +5,45 @@ config = {}
 config.hyper_fall_through = {'c', 'space', '\\', 'p', '\''}
 
 config.applications = {
-  {
+  ['iTerm'] = {
     name = 'iTerm',
     hyper_shortcut = 'j',
     preferred_display = 1
   },
-  {
+  ['Google Chrome'] = {
     name = 'Google Chrome',
     hyper_shortcut = 'k',
     preferred_display = 1
   },
-  {
+  ['Dash'] = {
     name = 'Dash',
     hyper_shortcut = 'h'
   },
-  {
+  ['Slack'] = {
     name = 'Slack',
     hyper_shortcut = 'i',
     preferred_display = 2
   },
-  {
+  ['Microsoft Outlook'] = {
     name = 'Microsoft Outlook',
     hyper_shortcut = 'e',
     preferred_display = 2
   },
-  {
+  ['Finder'] = {
     name = 'Finder',
     hyper_shortcut = 'f'
   },
-  {
+  ['Discord'] = {
     name = 'Discord',
     hyper_shortcut = 'l',
     preferred_display = 2
   },
-  {
+  ['zoom.us'] = {
     name = 'zoom.us',
     hyper_shortcut = 'z',
     preferred_display = 2
   },
-  {
+  ['Sketch'] = {
     name = 'Sketch',
     hyper_shortcut = 'v',
     preferred_display = 1
