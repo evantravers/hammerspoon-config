2c6cf7f87766861569817a19edf5ce3badb34dcb
Evan Travers <evantravers@gmail.com>
2020-05-21 21:47:28 -0500

    Keep changing things till it works
    I don't know why this works and the others didn't.
    Maybe because I don't require util in init?

--- util.lua
+++ util.lua
@@ -2,21 +2,21 @@ local fn = require('hs.fnutils')
 
 module = {}
 
-module.isTag = function(tag_or_bundle)
+local function isTag(tag_or_bundle)
   return string.find(tag_or_bundle, '#') == 1
 end
 
-module.appsTaggedWith = function(tag)
+local function appsTaggedWith(tag)
   return fn.filter(config.applications, function(app)
-    return app.tags and fn.contains(app.tags, tag_or_bundle)
+    return app.tags and fn.contains(app.tags, tag)
   end)
 end
 
 -- launches either by tag or by bundle id from a list
 module.launch = function(list)
   fn.map(list, function(tag_or_bundle)
-    if module.isTag(tag_or_bundle) then -- tag
-      fn.map(module.appsTaggedWith(tag_or_bundle), function(app)
+    if isTag(tag_or_bundle) then -- tag
+      fn.map(appsTaggedWith(tag_or_bundle), function(app)
         hs.application.launchOrFocusByBundleID(app.bundleID)
       end)
     else -- bundle
@@ -27,8 +27,8 @@ end
 
 module.kill = function(list)
   fn.map(list, function(tag_or_bundle)
-    if module.isTag(tag_or_bundle) then -- tag
-      fn.map(module.appsTaggedWith(tag_or_bundle), function(app)
+    if isTag(tag_or_bundle) then -- tag
+      fn.map(appsTaggedWith(tag_or_bundle), function(app)
         fn.map(hs.application.applicationsForBundleID(app.bundleID), function(app)
           app:kill()
         end)
@@ -41,4 +41,6 @@ module.kill = function(list)
   end)
 end
 
+module.isTag = isTag
+module.appsTaggedWith = appsTaggedWith
 return module
