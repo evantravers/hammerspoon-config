4296fea58f46ee224e3bb854453b419d24fe7f3a
Evan Travers <evantravers@gmail.com>
2020-06-05 10:34:56 -0500

    Remove requires
    (I'm hoping that cleans up memory and speeds things up.)

--- util.lua
+++ util.lua
@@ -1,26 +1,24 @@
-local fn = require('hs.fnutils')
-
-module = {}
+local module = {}
 
 local function appsTaggedWith(tag)
-  return fn.filter(config.applications, function(app)
-    return app.tags and fn.contains(app.tags, tag)
+  return hs.fnutils.filter(config.applications, function(app)
+    return app.tags and hs.fnutils.contains(app.tags, tag)
   end)
 end
 
 -- launches either by tag or by bundle id from a list
 module.launch = function(list)
-  fn.map(list, function(tag_or_bundle)
-    fn.map(appsTaggedWith(tag_or_bundle), function(app)
+  hs.fnutils.map(list, function(tag_or_bundle)
+    hs.fnutils.map(appsTaggedWith(tag_or_bundle), function(app)
       hs.application.launchOrFocusByBundleID(app.bundleID)
     end)
   end)
 end
 
 module.kill = function(list)
-  fn.map(list, function(tag_or_bundle)
-    fn.map(appsTaggedWith(tag_or_bundle), function(app)
-      fn.map(hs.application.applicationsForBundleID(app.bundleID), function(app)
+  hs.fnutils.map(list, function(tag_or_bundle)
+    hs.fnutils.map(appsTaggedWith(tag_or_bundle), function(app)
+      hs.fnutils.map(hs.application.applicationsForBundleID(app.bundleID), function(app)
         app:kill()
       end)
     end)
