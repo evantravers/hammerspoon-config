b75fafb6f8639bdee8f053bfdc191e08133d0ef3
Evan Travers <evantravers@gmail.com>
2020-06-06 16:47:23 -0500

    Majorly simplify secrets.
    Hey @megalithic... this means you can easily encode nested objects into
    your hs.settings while maintaining "secrecy".

--- secrets.lua
+++ secrets.lua
@@ -1,13 +1,7 @@
 module = {}
 
 module.start = function()
-  local file = io.open('.secrets')
-  io.input(file)
-
-  for line in io.lines() do
-    local data = hs.fnutils.split(line, " ")
-    hs.settings.set(data[1], data[2])
-  end
+  hs.settings.set("secrets", hs.json.read(".secrets.json"))
 end
 
 return module
