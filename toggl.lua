0357862113b86394b1498582a0733451232279ea
Evan Travers <evantravers@gmail.com>
2020-06-06 16:47:56 -0500

    Rewrite Toggl to support new secrets method.

--- toggl.lua
+++ toggl.lua
@@ -4,65 +4,86 @@
 
 local module = {}
 
-module.start_timer = function(project_id, description)
-  if hs.fnutils.contains(hs.settings.getKeys(), "toggl_key") then
-    local key = hs.settings.get('toggl_key')
-    hs.http.asyncPost(
-      "https://www.toggl.com/api/v8/time_entries/start",
-      hs.json.encode(
-        {
-          ['time_entry'] = {
-            ['description'] = description,
-            ['pid'] = project_id,
-            ['created_with'] = 'hammerspoon'
-          }
-        }
-      ),
-      {
-        ["Content-Type"] = "application/json; charset=UTF-8",
-        ["Authorization"] = "Basic " .. hs.base64.encode(key .. ":api_token")
-      },
-      function(http_number, body, headers)
-        print(http_number)
-        print(body)
-      end
-    )
+module.key = function()
+  if hs.settings.get("secrets").toggl_key then
+    return hs.settings.get("secrets").toggl_key
   else
-    print("You need to put a `toggl_key` in hs.settings.")
+    print("You need to load a Toggl.com API key in to hs.settings under secrets.toggl_key")
   end
 end
 
-module.current_timer = function()
-  if hs.fnutils.contains(hs.settings.getKeys(), "toggl_key") then
-    local key = hs.settings.get('toggl_key')
-    http_number, body, headers = hs.http.get(
-      "https://www.toggl.com/api/v8/time_entries/current",
+module.start_timer = function(project_id, description)
+  local key = module.key()
+  hs.http.asyncPost(
+    "https://www.toggl.com/api/v8/time_entries/start",
+    hs.json.encode(
       {
-        ["Content-Type"] = "application/json; charset=UTF-8",
-        ["Authorization"] = "Basic " .. hs.base64.encode(key .. ":api_token")
+        ['time_entry'] = {
+          ['description'] = description,
+          ['pid'] = project_id,
+          ['created_with'] = 'hammerspoon'
+        }
       }
-    )
-    if http_number == 200 then
-      if body == '{"data":null}' then
-        return nil
-      else
-        return hs.json.decode(body)
-      end
-    else
-      print("problems!")
+    ),
+    {
+      ["Content-Type"] = "application/json; charset=UTF-8",
+      ["Authorization"] = "Basic " .. hs.base64.encode(key .. ":api_token")
+    },
+    function(http_number, body, headers)
       print(http_number)
       print(body)
     end
+  )
+end
+
+module.current_timer = function()
+  local key = module.key()
+  http_number, body, headers = hs.http.get(
+    "https://www.toggl.com/api/v8/time_entries/current",
+    {
+      ["Content-Type"] = "application/json; charset=UTF-8",
+      ["Authorization"] = "Basic " .. hs.base64.encode(key .. ":api_token")
+    }
+  )
+  if http_number == 200 then
+    if body == '{"data":null}' then
+      return nil
+    else
+      return hs.json.decode(body)
+    end
   else
-    print("You need to put a `toggl_key` in hs.settings.")
+    print("problems!")
+    print(http_number)
+    print(body)
   end
 end
 
 module.get_project = function(pid)
-  if hs.fnutils.contains(hs.settings.getKeys(), "toggl_key") then
-    local key = hs.settings.get('toggl_key')
-    http_number, body, headers = hs.http.get(
-      "https://www.toggl.com/api/v8/projects/" .. pid,
+  local key = module.key()
+  http_number, body, headers = hs.http.get(
+    "https://www.toggl.com/api/v8/projects/" .. pid,
+    {
+      ["Content-Type"] = "application/json; charset=UTF-8",
+      ["Authorization"] = "Basic " .. hs.base64.encode(key .. ":api_token")
+    }
+  )
+  if http_number == 200 then
+    return hs.json.decode(body)
+  else
+    print("problems!")
+    print(http_number)
+    print(body)
+  end
+end
+
+module.stop_timer = function()
+  local current = module.current_timer()
+  if current then
+    local key = module.key()
+    http_number, body, headers = hs.http.doRequest(
+      "https://www.toggl.com/api/v8/time_entries/" .. current['data']['id'] .. "/stop",
+      "PUT",
+      nil,
       {
         ["Content-Type"] = "application/json; charset=UTF-8",
         ["Authorization"] = "Basic " .. hs.base64.encode(key .. ":api_token")
@@ -76,36 +97,7 @@ module.get_project = function(pid)
       print(body)
     end
   else
-    print("You need to put a `toggl_key` in hs.settings.")
-  end
-end
-
-module.stop_timer = function()
-  if hs.fnutils.contains(hs.settings.getKeys(), "toggl_key") then
-    local current = module.current_timer()
-    if current then
-      local key = hs.settings.get('toggl_key')
-      http_number, body, headers = hs.http.doRequest(
-        "https://www.toggl.com/api/v8/time_entries/" .. current['data']['id'] .. "/stop",
-        "PUT",
-        nil,
-        {
-          ["Content-Type"] = "application/json; charset=UTF-8",
-          ["Authorization"] = "Basic " .. hs.base64.encode(key .. ":api_token")
-        }
-      )
-      if http_number == 200 then
-        return hs.json.decode(body)
-      else
-        print("problems!")
-        print(http_number)
-        print(body)
-      end
-    else
-      print("No timer running!")
-    end
-  else
-    print("You need to put a `toggl_key` in hs.settings.")
+    print("No timer running!")
   end
 end
 
