8a1f8c3b739ac42202ea7c3c70166846f1f96559
Evan Travers <evantravers@gmail.com>
2020-06-07 16:36:41 -0500

    Conditional logic on spaces...
    All kinds of fanciness is possible...
    
    Only spaces at work, only spaces at home, only during certain events...

--- spaces/design.lua
+++ spaces/design.lua
@@ -1,7 +1,9 @@
-table.insert(config.spaces, {
-  text = "Design",
-  subText = "Iterating and collaborating on Design artifacts in Figma",
-  image = hs.image.imageFromAppBundle('com.figma.Desktop'),
-  only = {'#design'},
-  toggl_proj = config.projects.design
-})
+if hs.application.pathForBundleID('com.figma.Desktop') then
+  table.insert(config.spaces, {
+    text = "Design",
+    subText = "Iterating and collaborating on Design artifacts in Figma",
+    image = hs.image.imageFromAppBundle('com.figma.Desktop'),
+    only = {'#design'},
+    toggl_proj = config.projects.design
+  })
+end
