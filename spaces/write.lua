98355769f158fee7abc6c98fb30177f2ab31c784
Evan Travers <evantravers@gmail.com>
2020-06-12 15:24:40 -0500

    Categorize "write" as deep.

--- spaces/write.lua
+++ spaces/write.lua
@@ -2,5 +2,6 @@ table.insert(config.spaces, {
   text = "Write",
   subText = "You are allowed to do anything you want, as long as you write. (Deep)",
   image = hs.image.imageFromAppBundle('com.agiletortoise.Drafts-OSX'),
+  toggl_proj = config.projects.deep,
   only = {'#writing'}
 })
