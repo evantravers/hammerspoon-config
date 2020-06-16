a904fec91965e245f073284b9ca07276b4c3cf5c
Evan Travers <evantravers@gmail.com>
2020-06-16 12:06:48 -0500

    Distractionless writing mode

--- spaces/write.lua
+++ spaces/write.lua
@@ -3,5 +3,20 @@ table.insert(config.spaces, {
   subText = "You are allowed to do anything you want, as long as you write. (Deep)",
   image = hs.image.imageFromAppBundle('com.agiletortoise.Drafts-OSX'),
   toggl_proj = config.projects.deep,
-  only = {'#writing'}
+  only = {'#writing'},
+  setup = 'distractionless_writing'
 })
+
+config.setup.distractionless_writing = function()
+  local drafts = hs.application("Drafts")
+
+  drafts:selectMenuItem("Hide Draft List")
+  drafts:selectMenuItem("Hide Filters")
+  drafts:selectMenuItem("Hide Action List")
+  drafts:selectMenuItem("Hide Toolbar")
+  drafts:selectMenuItem("Hide Tag Entry")
+  drafts:selectMenuItem("Hide Action Bar")
+  -- if drafts:findMenuItem("Typewriter Scrolling").ticked then
+  --   drafts:selectMenuItem("Typewriter Scrolling")
+  -- end
+end
