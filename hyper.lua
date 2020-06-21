8cb8a1ed84607dfb4ec7f7fb973128c02575900e
Evan Travers <evantravers@gmail.com>
2020-06-20 21:22:51 -0500

    Refactor hyper
    The `:isFrontmost()` works more reliably... `hs.application.find()`
    returns a table, not a boolean.

--- hyper.lua
+++ hyper.lua
@@ -73,7 +73,7 @@ end
 -- }
 hyper.start = function(config_table)
   -- Use the hyper key with the application config to use the `hyper_key`
-  for _, app in pairs(config_table.applications) do
+  hs.fnutils.map(config_table.applications, function(app)
     -- Apps that I want to jump to
     if app.hyper_key then
       hyper:bind({}, app.hyper_key, function() hyper.launch(app); end)
@@ -83,22 +83,25 @@ hyper.start = function(config_table)
     -- and I press the shortcut, open the app and send the shortcut, otherwise
     -- just send the shortcut.
     if app.local_bindings then
-      for _, key in pairs(app.local_bindings) do
+      hs.fnutils.map(app.local_bindings, function(key)
         hyper:bind({}, key, nil, function()
-          if hs.application.find(app.bundleID) then
+          if hs.application.get(app.bundleID) then
             hs.eventtap.keyStroke({'cmd','alt','shift','ctrl'}, key)
           else
             hyper.launch(app)
             hs.timer.waitWhile(
-              function() return hs.application.find(app.bundleID) == nil end,
+              function()
+                return not hs.application.get(app.bundleID):isFrontmost()
+              end,
               function()
                 hs.eventtap.keyStroke({'cmd','alt','shift','ctrl'}, key)
-              end)
+              end
+            )
           end
         end)
-      end
+      end)
     end
-  end
+  end)
 end
 
 hyper.enable_blocking = function(self)
