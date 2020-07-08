5f43861e41da47075d7bba47a8afa498f09842d9
Evan Travers <evantravers@gmail.com>
2020-07-08 16:08:30 -0500

    Adding more keywords
    I like adding keywords to the `subText` because my fuzzy search matches
    it, giving me less categorization at the end of the day.

--- spaces/play.lua
+++ spaces/play.lua
@@ -1,6 +1,6 @@
 table.insert(config.spaces, {
   text = "Play",
-  subText = "Take a break, relax... but not for long.",
+  subText = "Take a break, read, relax... but not for long.",
   image = hs.image.imageFromAppBundle('com.valvesoftware.steam'),
   toggl_proj = config.projects.play
 })
