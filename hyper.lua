e11b8f60690dd0a0c48d9c4935b3da5a5edf8b69
Evan Travers <evantravers@gmail.com>
2020-08-27 10:10:45 -0500

    Arbitrary changes

--- hyper.lua
+++ hyper.lua
@@ -13,13 +13,8 @@
 
 local hyper = hs.hotkey.modal.new({}, nil)
 
-hyper.pressed = function()
-  hyper:enter()
-end
-
-hyper.released = function()
-  hyper:exit()
-end
+hyper.pressed  = function() hyper:enter() end
+hyper.released = function() hyper:exit() end
 
 -- Set the key you want to be HYPER to F19 in karabiner or keyboard
 -- Bind the Hyper key to the hammerspoon modal
