4e459c1c4d28943db638b8132807bdf9515e114d
Evan Travers <evantravers@gmail.com>
2020-11-12 10:03:15 -0600

    Open agenda for items during a meeting
    It's helpful to see if I have anything to share with people when I'm in
    a meeting.

--- spaces/collaboration.lua
+++ spaces/collaboration.lua
@@ -4,5 +4,15 @@ table.insert(config.spaces, {
   image = hs.image.imageFromAppBundle('com.flexibits.fantastical2.mac'),
   launch = {'calendar'},
   blacklist = {'distraction'},
-  toggl_proj = config.projects.meeting
+  toggl_proj = config.projects.meeting,
+  funcs = 'agendaFor'
 })
+
+config.funcs.agendaFor = {
+  setup = function()
+    hs.urlevent.openURL("things:///show?id=anytime&filter=@ProctorU,!AgendaFor")
+  end,
+  teardown = function()
+    hs.urlevent.openURL("things:///show?id=today")
+  end
+}
