e7e2a0014121541aba9437657a8281eede2b0377
Evan Travers <evantravers@gmail.com>
2021-02-06 20:01:13 -0600

    Make `magiclines` not global

--- init.lua
+++ init.lua
@@ -246,7 +246,7 @@ hyper:bind({}, 'v', nil, function()
 end)
 
 -- https://stackoverflow.com/questions/19326368/iterate-over-lines-including-blank-lines
-function magiclines(s)
+local function magiclines(s)
   if s:sub(-1)~="\n" then s=s.."\n" end
   return s:gmatch("(.-)\n")
 end
