e4c2512e763a3fdfe7a72be3f6f339bd7445a774
Evan Travers <evantravers@gmail.com>
2021-02-13 23:15:15 -0600

    Experiment: Single page Review
    I've been doing a two screen review, but I discovered today that
    holding `⌘` and clicking a link in Things opens it in a new window.
    (This also works for Quick Search!)
    
    This means that a lot of my Review tasks could open in a new window...
    I'm not sure how this will work for weekly, because I still would like
    the checklist, but I'm going to give it a whirl.

--- spaces/review.lua
+++ spaces/review.lua
@@ -8,8 +8,7 @@ table.insert(Config.spaces, {
   toggl_desc = "Review",
   blacklist = {'distraction', 'communication'},
   layouts = {
-    {"Things", "Inbox", hs.screen.primaryScreen():name(), hs.layout.left70, 0, 0},
-    {"Things", "Today", hs.screen.primaryScreen():name(), hs.layout.right30, 0, 0}
+    {"Things", "Today", hs.screen.primaryScreen():name(), hs.layout.maximized, 0, 0}
   }
 })
 
@@ -17,10 +16,6 @@ Config.funcs.review = {
   setup = function()
     local things = hs.application.find('com.culturedcode.ThingsMac')
     hs.fnutils.imap(things:allWindows(), function(v) v:close() end)
-    hs.urlevent.openURL("things:///show?id=inbox")
-    things:selectMenuItem("Show Sidebar")
-
-    things:selectMenuItem("New Things Window")
     hs.urlevent.openURL("things:///show?id=today")
     things:selectMenuItem("Hide Sidebar")
   end,
