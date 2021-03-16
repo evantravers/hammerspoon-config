9adf568adcf1ea29af1ce3936bf4951d5892ae00
Evan Travers <evantravers@gmail.com>
2021-03-16 11:08:36 -0500

    We use teams now...
    So focused meeting can't kill all communication tools.

--- spaces/focused_meeting.lua
+++ spaces/focused_meeting.lua
@@ -2,7 +2,7 @@ table.insert(Config.spaces, {
   text = "Focused Meeting",
   subText = "People come first.",
   image = hs.image.imageFromAppBundle('com.flexibits.fantastical2.mac'),
-  blacklist = {'distraction', 'communication'},
+  blacklist = {'distraction'},
   launch = {'calendar'},
   toggl_proj = Config.projects.focused_meeting,
 })
