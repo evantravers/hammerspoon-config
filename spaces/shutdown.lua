61ac1429d354a602b76d01020c482ece9bed1518
Evan Travers <evantravers@gmail.com>
2021-03-20 10:31:38 -0500

    Fix shutdown

--- spaces/shutdown.lua
+++ spaces/shutdown.lua
@@ -1,5 +1,3 @@
--- local toggl = require('toggl')
-
 table.insert(Config.spaces, {
   text = "Work is done.",
   subText = "End of the day, or going to lunch.",
@@ -8,7 +6,7 @@ table.insert(Config.spaces, {
 
 Config.funcs.shutdown = {
   setup = function()
-    -- toggl.stop_timer()
+    spoon.Headspace.stopToggl()
     hs.settings.clear("headspace")
 
     -- shut down everything
