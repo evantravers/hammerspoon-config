f9d199d53c079e5ec08271f1d1e176fae9fb2f71
Evan Travers <evantravers@gmail.com>
2021-03-29 09:31:14 -0500

    Start documenting Hyper to pull it out as a spoon

--- hyper.lua
+++ hyper.lua
@@ -1,15 +1,15 @@
--- HYPER
---
--- Hyper is a hyper shortcut modal.
---
--- Feel free to modify... I use karabiner-elements.app on my laptop and QMK on
--- my mech keyboards to bind a single key to `F19`, which fires all this
--- Hammerspoon-powered OSX automation.
---
--- I chiefly use it to launch applications quickly from a single press,
--- although I also use it to create "universal" local bindings inspired by
--- Shawn Blanc's OopsieThings.
--- https://thesweetsetup.com/oopsiethings-applescript-for-things-on-mac/
+--- === HYPER ===
+---
+--- Hyper is a hyper shortcut modal.
+---
+--- Feel free to modify... I use karabiner-elements.app on my laptop and QMK on
+--- my mech keyboards to bind a single key to `F19`, which fires all this
+--- Hammerspoon-powered OSX automation.
+---
+--- I chiefly use it to launch applications quickly from a single press,
+--- although I also use it to create "universal" local bindings inspired by
+--- Shawn Blanc's OopsieThings.
+--- https://thesweetsetup.com/oopsiethings-applescript-for-things-on-mac/
 
 local hyper = hs.hotkey.modal.new({}, nil)
 
