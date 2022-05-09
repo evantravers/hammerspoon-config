51ebb95d4868b3a4d618ba200af676333a9aa1a4
Evan Travers <evantravers@gmail.com>
2022-05-09 15:17:46 -0500

    Add miro

--- init.lua
+++ init.lua
@@ -215,7 +215,7 @@ end)
 Hyper:bind({}, 'v', nil, function()
   if hs.application.find('com.figma.Desktop') then
     hs.application.launchOrFocusByBundleID('com.figma.Desktop')
-  if hs.application.find('com.electron.realtimeboard') then
+  elseif hs.application.find('com.electron.realtimeboard') then
     hs.application.launchOrFocusByBundleID('com.electron.realtimeboard')
   elseif hs.application.find('com.adobe.LightroomClassicCC7') then
     hs.application.launchOrFocusByBundleID('com.adobe.LightroomClassicCC7')
