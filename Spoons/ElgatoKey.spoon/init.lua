9e440f42a54a7a4a5f5a88754091a6b5d4cc6a71
Evan Travers <evantravers@gmail.com>
2022-06-16 14:14:27 -0500

    Fixing ElgatoKey
    - using some callbacks to refresh if not found
    - simplifying the result of `service:addresses()`

--- Spoons/ElgatoKey.spoon/init.lua
+++ Spoons/ElgatoKey.spoon/init.lua
@@ -9,33 +9,51 @@ local M = {
 }
 
 function M:start()
+  M.cache = {}
+  M:findServices()
+
+  return self
+end
+
+function M:findServices()
   local browser = hs.bonjour.new()
 
   browser:findServices('_elg._tcp', function(browserObject, domain, advertised, service, terminated)
     M.service = service
     service:resolve(function(serviceObj, result)
       if result == "resolved" then
-        M.ip = hs.fnutils.find(service:addresses(), function(addr)
-          return addr:match("%d%d%d.%d%d%d.%d.%d+")
-        end)
-        M.port = service:port()
+        print("yey")
+        M.cache.ip = service:addresses()[1]
+        M.cache.port = service:port()
         browserObject:stop()
+
+        return M.cache
       end
     end)
   end)
+end
 
-  return self
+function M:ip()
+  if M.cache.ip then
+    return M.cache.ip
+  else
+    return M:findServices()["ip"]
+  end
+end
+
+function M:port()
+  if M.cache.port then
+    return M.cache.port
+  else
+    return M:findServices()["port"]
+  end
 end
 
 local function onOff(i)
-  if not M.ip then
-    M:start()
-  end
-
-  local status, body, headers = hs.http.get("http://" .. M.ip .. ":" .. M.port .. "/elgato/lights")
+  local status, body, headers = hs.http.get("http://" .. M:ip() .. ":" .. M:port() .. "/elgato/lights")
   local settings = hs.json.decode(body)
   settings.lights[1].on = i
-  local status, response, header = hs.http.doRequest("http://" .. M.ip .. ":" .. M.port .. "/elgato/lights", "PUT", hs.json.encode(settings))
+  local status, response, header = hs.http.doRequest("http://" .. M.ip() .. ":" .. M:port() .. "/elgato/lights", "PUT", hs.json.encode(settings))
 end
 
 function M:off()
