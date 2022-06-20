9fe791ab8a25973d5713e1d2847d617570696320
Evan Travers <evantravers@gmail.com>
2022-06-20 16:52:41 -0500

    I don't use Things for the checklist anymore

--- spaces/review.lua
+++ spaces/review.lua
@@ -8,23 +8,14 @@ table.insert(Config.spaces, {
   togglDesc = "Review",
   blacklist = {'distraction', 'communication'},
   layouts = {
-    {"Things", "Today", hs.screen.primaryScreen():name(), hs.layout.right30, 0, 0},
-    {"Obsidian", nil, hs.screen.primaryScreen():name(), hs.layout.left70, 0, 0}
+    {"Obsidian", nil, hs.screen.primaryScreen():name(), hs.layout.maximized, 0, 0}
   }
 })
 
 Config.funcs.review = {
   setup = function()
-    local things = hs.application.find('com.culturedcode.ThingsMac')
-    hs.fnutils.imap(things:allWindows(), function(v) v:close() end)
-    hs.urlevent.openURL("things:///show?id=today")
-    things:selectMenuItem("Hide Sidebar")
     hs.urlevent.openURL("obsidian://advanced-uri?vault=wiki&commandname=Periodic%20Notes%3A%20Open%20daily%20note")
   end,
   teardown = function()
-    local things = hs.application.find('com.culturedcode.ThingsMac')
-    if things then
-      things:selectMenuItem("Update with Things Cloud")
-    end
   end
 }
