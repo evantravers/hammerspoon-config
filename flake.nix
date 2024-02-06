217d458b872cc6b58c6567c41e4cae4e09f7c47f
Evan Travers <evantravers@gmail.com>
2024-02-06 16:36:44 -0600

    Use devshell
    🤷

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "virtual environments";
+
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+
+  outputs = { self, flake-utils, devshell, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShells.default =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+
+            overlays = [ devshell.overlays.default ];
+          };
+        in
+        pkgs.devshell.mkShell {
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+        };
+    });
+}
