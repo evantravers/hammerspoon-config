bf4a806a367196b3209a89501d936f9700437eb2
Evan Travers <evantravers@gmail.com>
2024-02-07 09:29:36 -0600

    tabstop

--- init.lua
+++ init.lua
@@ -36,27 +36,27 @@ end
 
 -- https://github.com/dmitriiminaev/Hammerspoon-HyperModal/blob/master/.hammerspoon/yabai.lua
 local yabai = function(args, completion)
-    local yabai_output = ""
-    local yabai_error = ""
-    -- Runs in background very fast
-    local yabai_task = hs.task.new("/run/current-system/sw/bin/yabai", function(err, stdout, stderr)
-        print()
-    end, function(task, stdout, stderr)
-        -- print("stdout:"..stdout, "stderr:"..stderr)
-        if stdout ~= nil then
-            yabai_output = yabai_output .. stdout
-        end
-        if stderr ~= nil then
-            yabai_error = yabai_error .. stderr
-        end
-        return true
+  local yabai_output = ""
+  local yabai_error = ""
+  -- Runs in background very fast
+  local yabai_task = hs.task.new("/run/current-system/sw/bin/yabai", function(err, stdout, stderr)
+    print()
+  end, function(task, stdout, stderr)
+      -- print("stdout:"..stdout, "stderr:"..stderr)
+      if stdout ~= nil then
+        yabai_output = yabai_output .. stdout
+      end
+      if stderr ~= nil then
+        yabai_error = yabai_error .. stderr
+      end
+      return true
     end, args)
-    if type(completion) == "function" then
-        yabai_task:setCallback(function()
-            completion(yabai_output, yabai_error)
-        end)
-    end
-    yabai_task:start()
+  if type(completion) == "function" then
+    yabai_task:setCallback(function()
+      completion(yabai_output, yabai_error/run/current-system/sw/bin/yabai)
+    end)
+  end
+  yabai_task:start()
 end
 
 HyperModal = spoon.HyperModal
